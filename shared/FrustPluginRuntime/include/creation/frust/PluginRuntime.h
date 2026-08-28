c85914f8c5ba45066dec8b8262ea279017bb67f2
William Westlake <wwestlake@lagdaemon.com>
2026-08-27 22:32:29 -0400

    Extend shared FRust runtime lifecycle dispatch

--- shared/FrustPluginRuntime/include/creation/frust/PluginRuntime.h
+++ shared/FrustPluginRuntime/include/creation/frust/PluginRuntime.h
@@ -2,6 +2,7 @@
 
 #include <frust_plugin_host/FrustPluginHost.h>
 
+#include <cstdint>
 #include <string>
 
 namespace creation::frust
@@ -17,8 +18,11 @@ public:
 
     void registerHostFunction(const char* name, void* function);
     bool load(const std::string& pluginPath, std::string& error);
+    bool reload(std::string& error);
     void unload();
     [[nodiscard]] void* getFunction(const char* name) const;
+    [[nodiscard]] std::int64_t callEvent(std::int64_t id, std::int64_t argument) const;
+    void fireEvent(const char* name, void* payload = nullptr) const;
     [[nodiscard]] bool isLoaded() const noexcept;
 
 private:
